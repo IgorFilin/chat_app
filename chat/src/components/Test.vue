3d846e9bd839f8ca9814022f5d5031bb7d5df4f9
IgorFilin <filinigor@yandex.ru>
2023-12-22 20:34:54 +0300

    Билд с обновленными переменными окружения

--- chat/src/components/Test.vue
+++ chat/src/components/Test.vue
@@ -42,7 +42,9 @@ const inputs = [
     id: 'password',
   },
 ];
-onUpdated(() => console.log(textValue.value));
+onUpdated(() => {
+  console.log(textValue.value);
+});
 </script>
 <style lang="scss">
 .test {
