2d831668928a444adf8153f782c56e0d12052a53
IgorFilin <filinigor@yandex.ru>
2023-12-25 19:35:55 +0300

    Смена цвета основного лоадера

--- chat/src/components/Loader.vue
+++ chat/src/components/Loader.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="v-loader_container" :class="loaderFor">
+  <div
+    class="v-loader_container"
+    :class="loaderFor">
     <span :class="setClass"></span>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed } from 'vue';
 
 const props = defineProps({
   loaderFor: {
     type: String,
-    desc: "Класс для выбора нужного лоадера",
-    default: "main",
+    desc: 'Класс для выбора нужного лоадера',
+    default: 'main',
   },
 });
 
 const setClass = computed(() => {
   switch (props.loaderFor) {
-    case "message":
-      return "v-loader__message";
+    case 'message':
+      return 'v-loader__message';
     default:
-      return "v-loader__main";
+      return 'v-loader__main';
   }
 });
 </script>
@@ -37,7 +39,7 @@ const setClass = computed(() => {
   &.main {
     position: absolute;
     top: 0;
-    background-color: white;
+    background-color: #090909;
   }
 
   &.message {
@@ -47,19 +49,18 @@ const setClass = computed(() => {
 .v-loader__main {
   width: 48px;
   height: 24px;
-  color: #fff;
   background: currentColor;
   border-radius: 50% 50% 0 0;
   position: relative;
   margin: auto;
-  color: violet;
+  color: #0f0;
   display: block;
   margin: 60px auto 0;
   box-sizing: border-box;
   animation: animloader 4s linear infinite;
 
   &::after {
-    content: "";
+    content: '';
     position: absolute;
     left: 50%;
     transform: translateX(-50%);
@@ -80,23 +81,17 @@ const setClass = computed(() => {
       box-shadow: 40px -1px 0 -2px, 0 0 0 -2px, 40px -1px 0 -5px, 0 0 0 -5px;
     }
     40% {
-      box-shadow: 40px -1px 0 -2px, -40px -1px 0 -2px, 40px -1px 0 -5px,
-        -40px -1px 0 -5px;
+      box-shadow: 40px -1px 0 -2px, -40px -1px 0 -2px, 40px -1px 0 -5px, -40px -1px 0 -5px;
     }
     60% {
-      box-shadow: 40px -1px 0 -2px, -40px -1px 0 -2px, 23px -29px 0 -5px,
-        -40px -1px 0 -5px;
+      box-shadow: 40px -1px 0 -2px, -40px -1px 0 -2px, 23px -29px 0 -5px, -40px -1px 0 -5px;
     }
     80%,
     95% {
-      box-shadow: 40px -1px 0 -2px, -40px -1px 0 -2px, 23px -29px 0 -5px,
-        -23px -29px 0 -5px;
+      box-shadow: 40px -1px 0 -2px, -40px -1px 0 -2px, 23px -29px 0 -5px, -23px -29px 0 -5px;
     }
     100% {
-      box-shadow: 40px -1px 0 -2px rgba(255, 255, 255, 0),
-        -40px -1px 0 -2px rgba(255, 255, 255, 0),
-        23px -29px 0 -5px rgba(255, 255, 255, 0),
-        -23px -29px 0 -5px rgba(255, 255, 255, 0);
+      box-shadow: 40px -1px 0 -2px rgba(255, 255, 255, 0), -40px -1px 0 -2px rgba(255, 255, 255, 0), 23px -29px 0 -5px rgba(255, 255, 255, 0), -23px -29px 0 -5px rgba(255, 255, 255, 0);
     }
   }
 
@@ -108,23 +103,17 @@ const setClass = computed(() => {
       box-shadow: 40px 2px 0 -2px, 0 0 0 -2px, 40px 2px 0 -5px, 0 0 0 -5px;
     }
     40% {
-      box-shadow: 40px 2px 0 -2px, -40px 2px 0 -2px, 40px 2px 0 -5px,
-        -40px 2px 0 -5px;
+      box-shadow: 40px 2px 0 -2px, -40px 2px 0 -2px, 40px 2px 0 -5px, -40px 2px 0 -5px;
     }
     60% {
-      box-shadow: 40px 2px 0 -2px, -40px 2px 0 -2px, 23px -23px 0 -5px,
-        -40px 2px 0 -5px;
+      box-shadow: 40px 2px 0 -2px, -40px 2px 0 -2px, 23px -23px 0 -5px, -40px 2px 0 -5px;
     }
     80%,
     95% {
-      box-shadow: 40px 2px 0 -2px, -40px 2px 0 -2px, 23px -23px 0 -5px,
-        -23px -23px 0 -5px;
+      box-shadow: 40px 2px 0 -2px, -40px 2px 0 -2px, 23px -23px 0 -5px, -23px -23px 0 -5px;
     }
     100% {
-      box-shadow: 40px 2px 0 -2px rgba(255, 255, 255, 0),
-        -40px 2px 0 -2px rgba(255, 255, 255, 0),
-        23px -23px 0 -5px rgba(255, 255, 255, 0),
-        -23px -23px 0 -5px rgba(255, 255, 255, 0);
+      box-shadow: 40px 2px 0 -2px rgba(255, 255, 255, 0), -40px 2px 0 -2px rgba(255, 255, 255, 0), 23px -23px 0 -5px rgba(255, 255, 255, 0), -23px -23px 0 -5px rgba(255, 255, 255, 0);
     }
   }
 }
@@ -138,15 +127,13 @@ const setClass = computed(() => {
   animation: envFloating 1s ease-in infinite alternate;
 
   &:after {
-    content: "";
+    content: '';
     position: absolute;
     right: 62px;
     top: -40px;
     height: 70px;
     width: 50px;
-    background-image: linear-gradient(#fff 45px, transparent 0),
-      linear-gradient(#fff 45px, transparent 0),
-      linear-gradient(#fff 45px, transparent 0);
+    background-image: linear-gradient(#fff 45px, transparent 0), linear-gradient(#fff 45px, transparent 0), linear-gradient(#fff 45px, transparent 0);
     background-repeat: no-repeat;
     background-size: 30px 4px;
     background-position: 0px 11px, 8px 35px, 0px 60px;
