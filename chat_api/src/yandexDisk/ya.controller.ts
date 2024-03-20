e48d09c72a9761e2e1378020bf4110c1222e515f
IgorFilin <filinigor@yandex.ru>
2024-03-20 22:46:37 +0300

    Работа над получением музыки с яндекс диска

--- chat_api/src/yandexDisk/ya.controller.ts
+++ chat_api/src/yandexDisk/ya.controller.ts
@@ -1,34 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { YandexDiskConnectorService } from './ya.service';
-import { HttpService } from '@nestjs/axios';
 import { catchError, firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
-import { ConfigService } from '@nestjs/config';
+import { YandexDiskConnectorService } from './ya.service';
 
 @Controller('ya_disk')
 export class YandexDiskConnectorController {
-  constructor(
-    private readonly YandexDiskConnectorService: YandexDiskConnectorService,
-    private readonly httpService: HttpService,
-    private readonly configService: ConfigService
-  ) {}
+  constructor(private readonly yandexDiskConnectorService: YandexDiskConnectorService) {}
 
   @Get('get_resourse')
-  async connectionYaDisk() {
-    const yaKey = this.configService.get('YA_DISK_AUTH');
-    const { data } = await firstValueFrom(
-      this.httpService
-        .get('https://cloud-api.yandex.net/v1/disk/', {
-          headers: {
-            Authorization: `OAuth ${yaKey}`,
-          },
-        })
-        .pipe(
-          catchError((error: AxiosError) => {
-            throw error;
-          })
-        )
-    );
-    console.log(data);
+  async connectionYaDiskResources(media_type: string = 'audio', fields: string, limit: string, offset: string) {
+    const result = await this.yandexDiskConnectorService.connectionYaDisk(media_type, fields, limit, offset);
+    console.log(result);
   }
 }
--- chat_api/src/yandexDisk/ya.service.ts
+++ chat_api/src/yandexDisk/ya.service.ts
@@ -1,4 +1,42 @@
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class YandexDiskConnectorService {}
+export class YandexDiskConnectorService {
+  constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {}
+
+  yaKey = this.configService.get('YA_DISK_AUTH');
+
+  instanceHeaders = {
+    Authorization: `OAuth ${this.yaKey}`,
+  };
+
+  async connectionYaDisk(media_type: string = 'audio', fields: string, limit: string, offset: string) {
+    const queryObj = {
+      media_type,
+      fields,
+      limit,
+      offset,
+    };
+    for (const key in queryObj) {
+      if (!queryObj[key]) delete queryObj[key];
+    }
+    const queryParams = new URLSearchParams(queryObj);
+    console.log(queryParams.toString());
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get(`https://cloud-api.yandex.net/v1/disk/resources/files?${queryParams.toString()}`, {
+          headers: this.instanceHeaders,
+        })
+        .pipe(
+          catchError((error: AxiosError) => {
+            throw error;
+          })
+        )
+    );
+    return data;
+  }
+}
