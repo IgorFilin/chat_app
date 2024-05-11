238163a8c660c94e409adcec856329be2e711d67
IgorFilin <filinigor@yandex.ru>
2024-05-12 00:44:07 +0300

    Фикс стилей попапа приглашения

--- chat/src/components/MainPage.vue
+++ chat/src/components/MainPage.vue
@@ -297,6 +297,12 @@ onUnmounted(() => {
   position: absolute;
   min-width: 350px;
   transform: translate3d(0, 0, 0);
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  height: fit-content;
 
   &.popup-enter-active {
     @include animation(open-popup);
