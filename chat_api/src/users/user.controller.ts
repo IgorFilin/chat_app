2d898b24906daa0de96e9c87f6d051419ebc5dc9
IgorFilin <filinigor@yandex.ru>
2024-05-26 18:07:01 +0300

    Просмотр квери параметров

--- chat_api/src/users/user.controller.ts
+++ chat_api/src/users/user.controller.ts
@@ -129,7 +129,7 @@ export class UsersController {
   @Get('validateCaptcha')
   async captcha(@Req() req: Request, @Res() res: Response) {
     const token = req.query.token;
-    console.log('=+=', req);
+    console.log('=+=', req.query);
 
     function check_captcha(token, callback) {
       const options = {
