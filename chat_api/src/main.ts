51e3823b2a7d0af4c87d301549cca0af966b7ab6
IgorFilin <filinigor@yandex.ru>
2024-05-26 20:46:20 +0300

    Откати изменений

--- chat_api/src/main.ts
+++ chat_api/src/main.ts
@@ -2,24 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import * as cookieParser from 'cookie-parser';
-import fs from 'node:fs';
-import express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import https from 'https';
-import { ExtendedSocketIoAdapter } from './websocketExtendedAdapter/ExtendedSocketIoAdapter';
 
 async function bootstrap() {
-  const privateKey = fs.readFileSync('/etc/letsencrypt/live/filin-hub.online/privkey.pem', 'utf8');
-  const certificate = fs.readFileSync('/etc/letsencrypt/live/filin-hub.online/fullchain.pem', 'utf8');
-
-  const httpsOptions = { key: privateKey, cert: certificate };
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  const httpsServer = https.createServer(httpsOptions);
-
+  const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({ origin: true, credentials: true });
-  app.useWebSocketAdapter(new ExtendedSocketIoAdapter(httpsServer));
+  app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
