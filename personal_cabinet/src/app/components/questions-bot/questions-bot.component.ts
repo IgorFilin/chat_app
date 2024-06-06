ccd32be6852bf0479e7965241427f88986155402
IgorFilin <filinigor@yandex.ru>
2024-06-06 23:35:37 +0300

    Добавление ответов, возможности добавления ответов, получения данных

--- personal_cabinet/src/app/components/questions-bot/questions-bot.component.ts
+++ personal_cabinet/src/app/components/questions-bot/questions-bot.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'cabinet-questions-bot',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './questions-bot.component.html',
-  styleUrl: './questions-bot.component.scss'
+  styleUrl: './questions-bot.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QuestionsBotComponent {
+  createQuestionForm: FormGroup = new FormGroup({
+    question: new FormControl(''),
+    answer_0: new FormControl(''),
+  });
 
+  variantAnswers: Array<string> = ['answer_0'];
+
+  onHandlerClickAddQuestions() {
+    const newAnswer = `answer_${this.variantAnswers.length - 1}`;
+    this.variantAnswers.push(newAnswer);
+    this.createQuestionForm.addControl(newAnswer, new FormControl(''));
+  }
+
+  onSubmit() {
+    console.log(this.createQuestionForm.value);
+  }
 }
