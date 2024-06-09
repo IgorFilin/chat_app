25d6e9d66bd8c1a44ad6d85645b8b8eb96769d37
IgorFilin <filinigor@yandex.ru>
2024-06-09 16:18:07 +0300

    Работа над формой

--- personal_cabinet/src/app/components/questions-bot/questions-bot.component.ts
+++ personal_cabinet/src/app/components/questions-bot/questions-bot.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 
@@ -16,23 +18,39 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QuestionsBotComponent {
-  createQuestionForm: FormGroup = new FormGroup({
-    question: new FormControl('', [Validators.required]),
-    answer_0: new FormControl('', [Validators.required]),
-  });
+  createQuestionForm: FormGroup = new FormGroup(
+    {
+      question: new FormControl('', [Validators.required]),
+      answer_0: new FormControl('', [Validators.required]),
+    },
+    Validators.required
+  );
 
   variantAnswers: Array<string> = ['answer_0'];
 
   onHandlerClickAddQuestions() {
-    const newAnswer = `answer_${this.variantAnswers.length - 1}`;
+    const newAnswer = `answer_${this.variantAnswers.length}`;
     this.variantAnswers.push(newAnswer);
     this.createQuestionForm.addControl(
       newAnswer,
       new FormControl('', [Validators.required])
     );
+    this.createQuestionForm.updateValueAndValidity();
+  }
+
+  onHandlerClickRemoveQuestions() {
+    const removeControl = this.variantAnswers.pop();
+    if (removeControl) {
+      this.createQuestionForm.removeControl(removeControl);
+      this.createQuestionForm.updateValueAndValidity();
+    }
   }
 
   onSubmit() {
+    for (const formValueKey in this.createQuestionForm.value) {
+      this.createQuestionForm.value[formValueKey] =
+        this.createQuestionForm.value[formValueKey].trim();
+    }
     console.log(this.createQuestionForm.value);
   }
 }
--- personal_cabinet/src/app/core/services/toaster.service.ts
+++ personal_cabinet/src/app/core/services/toaster.service.ts
@@ -7,19 +7,23 @@ import { ToastrService } from 'ngx-toastr';
 export class ToasterService {
   constructor(private toastr: ToastrService) {}
 
+  globalOptions: any = {
+    positionClass: 'toast-bottom-left',
+  };
+
   success(message: string, title?: string) {
-    this.toastr.success(message, title);
+    this.toastr.success(message, title, this.globalOptions);
   }
 
   error(message: string, title?: string) {
-    this.toastr.error(message, title);
+    this.toastr.error(message, title, this.globalOptions);
   }
 
   warning(message: string, title?: string) {
-    this.toastr.warning(message, title);
+    this.toastr.warning(message, title, this.globalOptions);
   }
 
   info(message: string, title?: string) {
-    this.toastr.info(message, title);
+    this.toastr.info(message, title, this.globalOptions);
   }
 }
