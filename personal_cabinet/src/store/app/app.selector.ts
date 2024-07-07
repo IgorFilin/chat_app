15469fbf43f2a223abc22b956a8a0c4099dd59b8
IgorFilin <filinigor@yandex.ru>
2024-07-07 19:44:50 +0300

    Временный тест без кабинета

--- personal_cabinet/src/store/app/app.selector.ts
+++
@@ -1,9 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { InitialAppStateType } from './app.reducer';
-
-export const SelectAppState = createFeatureSelector<InitialAppStateType>('app');
-
-export const GetIsStatusMenu = createSelector(
-  SelectAppState,
-  (state: InitialAppStateType) => state.isOpenMenu
-);
--- personal_cabinet/src/store/auth/types.ts
+++
@@ -1,5 +0,0 @@
-export interface ResponseDataRegistrationType {
-  email: string;
-  isAcceptKey: boolean;
-  message: string;
-}
