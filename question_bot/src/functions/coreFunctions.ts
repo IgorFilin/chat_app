8071cc130356563be6c2820733613c8a406d7367
IgorFilin <filinigor@yandex.ru>
2024-07-26 00:13:48 +0300

    Добавление логов, тест

--- question_bot/src/functions/coreFunctions.ts
+++ question_bot/src/functions/coreFunctions.ts
@@ -2,14 +2,17 @@ import { apiBot } from '../api/api';
 import { Markup } from 'telegraf';
 
 export async function getQuestionsData(): Promise<any> {
+  console.log('getQuestionsData inside');
   return await apiBot.getQuestions();
 }
 
 export function createButtonsAnswer(question: any) {
+  console.log('createButtonsAnswer inside', question);
   return Markup.inlineKeyboard(question.answers.map((answer: any) => Markup.button.callback(answer.content, `id=${answer.id};isAccept=${answer.isAccept}`)));
 }
 
 export async function initialize(bot: any) {
+  console.log('initialize inside');
   const data = await getQuestionsData();
 
   const question = data[0];
--- question_bot/src/index.ts
+++ question_bot/src/index.ts
@@ -8,16 +8,17 @@ import { initialize } from './functions/coreFunctions';
 const botToken: string = process.env.BOT_TOKEN!;
 
 const bot = new Telegraf(botToken);
-
+console.log(1);
 // Настройка вебхука
 const PORT = 4000;
 const URL = process.env.URL || 'https://filin-hub.online';
-
+console.log(2, URL);
 // Настройка webhook
 bot.telegram.setWebhook(`${URL}/bot/`);
 app.use(bot.webhookCallback('/bot/'));
 
 bot.action('callback_query', async (ctx: any) => {
+  console.log('callback_query', ctx);
   const regx = /id=([^\s;]+);isAccept=(true|false)/;
   const data = ctx.data;
   const id = data.match(regx)[1];
@@ -26,6 +27,7 @@ bot.action('callback_query', async (ctx: any) => {
 });
 
 bot.launch().then(() => {
+  console.log('initialize');
   initialize(bot);
 });
 
