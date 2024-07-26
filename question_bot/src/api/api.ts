9a89114a698776fde491beb7b88d2d34db696bd7
IgorFilin <filinigor@yandex.ru>
2024-07-26 23:19:55 +0300

    Добавление api

--- question_bot/src/api/api.ts
+++ question_bot/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const mainInstance = axios.create({
-  baseURL: process.env.URL || 'https://filin-hub.online',
+  baseURL: process.env.URL || 'https://filin-hub.online/api/',
   withCredentials: true,
 });
 
