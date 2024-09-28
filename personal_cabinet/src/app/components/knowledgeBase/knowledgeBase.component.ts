8b85a0e2cdec016d9bdd9608fa623003cc789a27
IgorFilin <filinigor@yandex.ru>
2024-09-28 21:56:05 +0300

    Добавление библиотеки Prism

--- personal_cabinet/src/app/components/knowledgeBase/knowledgeBase.component.ts
+++ personal_cabinet/src/app/components/knowledgeBase/knowledgeBase.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionAnswerService } from '../../services/question-answer.service';
 import { CommonModule } from '@angular/common';
+import { TransformHtmlInCodePipe } from '../../pipes/transform-html-in-code.pipe';
 
 @Component({
   standalone: true,
   selector: 'app-knowledgeBase',
   templateUrl: './knowledgeBase.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, TransformHtmlInCodePipe],
   styleUrls: ['./knowledgeBase.component.scss'],
 })
 export class KnowledgeBaseComponent implements OnInit {
