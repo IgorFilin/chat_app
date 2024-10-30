075b091a55db1ddacc4afe8c1a07e513ea3aa085
IgorFilin <filinigor@yandex.ru>
2024-10-30 22:56:18 +0300

    Работа над textarea

--- personal_cabinet/src/app/components/markdown-textarea/markdown-textarea.component.ts
+++ personal_cabinet/src/app/components/markdown-textarea/markdown-textarea.component.ts
@@ -7,6 +7,12 @@ import { AbstractControlComponent } from '../../shared/components/abstract-contr
 import { IconComponent } from '../../shared/components/icon/icon.component';
 import { bubbleAnimation } from '../../animations/bubble.animation';
 
+export enum TransformText {
+  CODE = 'code',
+  BLOCK_CODE = 'block-code',
+  LINE = 'line',
+}
+
 @Component({
   standalone: true,
   selector: 'app-markdown-textarea',
@@ -24,44 +30,74 @@ import { bubbleAnimation } from '../../animations/bubble.animation';
 })
 export class MarkdownTextareaComponent extends AbstractControlComponent  {
   
-  copyedText: {start:number, end: number, text: string} | null = null;
+  copyedText: {start: number, end: number, text: string} | null = null;
   codeFormsToggle: boolean = false;
   fieldName: string = this.control?.name as string;
   isPreWatch: boolean = false
 
 
-  getSelectedText(event: MouseEvent) {
+  getSelectedText(event: MouseEvent | Event) {
     const textarea = event.target as HTMLTextAreaElement;
     const start = textarea.selectionStart;
     const end = textarea.selectionEnd;
-    if (start - end) {
+    // if (start - end) {
       this.copyedText = {
         start,
         end,
         text: textarea.value.substring(start, end),
       };
-    }
+    // }
   }
   
   onPreWatchHandler() {
     this.isPreWatch = !this.isPreWatch;
   }
 
-  onTransformTestInCode() {
-    if(!this.copyedText) return
-   
-    const transformedText = `\`\`\`typescript \n ${this.copyedText.text} \n \`\`\` \n`;
-    const beforeString = this.value.substring(0, this.copyedText['start']);
-    const afterString = this.value.substring(
-        this.copyedText['end'],
-        this.value.length
-      );
-    const result = `${beforeString} ${transformedText} ${afterString}`;
-    console.log(result);
+  onTransformTest(mode: any) {
+    let transformedText = ''
+    let result = '';
+    switch (mode) {
+      case TransformText.BLOCK_CODE:{
+        transformedText = `\`\`\`typescript \n ${this.copyedText?.text} \n \`\`\` \n`;
+        const beforeString = this.value.substring(0, this.copyedText!['start']);
+        const afterString = this.value.substring(
+            this.copyedText!['end'],
+            this.value.length
+          );
+        result = `${beforeString} ${transformedText} ${afterString}`;
+        break;
+      }
+      case TransformText.CODE:{
+        transformedText = `\`\`\` ${this.copyedText?.text} \`\`\``;
+        const beforeString = this.value.substring(0, this.copyedText!['start']);
+        const afterString = this.value.substring(
+            this.copyedText!['end'],
+            this.value.length
+          );
+        result = `${beforeString} ${transformedText} ${afterString}`;
+        break;
+      }
+      case TransformText.LINE:{
+        transformedText = `***`;
+        const beforeString = this.value.substring(0, this.copyedText!['start']);
+        const afterString = this.value.substring(
+            this.copyedText!['start'],
+            this.value.length
+          );
+        result = `${beforeString} ${transformedText} ${afterString}`;
+        console.log(result);
+        break;
+      }
+    }
+
     this.value = result;
     this.onChange(this.value);
     this.copyedText = null
     // console.log(1) - это консоль лог
+  }
+
+  onTextareaChange(event: Event) { 
+    this.getSelectedText(event)
   }
 
   onChangeModeHandler() {
