a479cdf35ec5d4499e7731a2d25b4cf12f04d233
IgorFilin <filinigor@yandex.ru>
2025-01-05 13:38:20 +0300

    Добавление создателя статьи на детальной странице

--- personal_cabinet/src/app/models/interfaces.ts
+++ personal_cabinet/src/app/models/interfaces.ts
@@ -1,58 +1,62 @@
-import { RoleType } from "./types";
+import { RoleType } from './types';
 
 export interface ITag {
-    id: string;
-    title: string;
+  id: string;
+  title: string;
 }
 
 export interface IArticleResponse {
+  id: string;
+  title: string;
+  description: string;
+  theme: string;
+  date: string;
+  tags: ITag[];
+  views: Array<{
     id: string;
-    title: string;
-    description: string;
-    theme: string;
-    date: string;
-    tags: ITag[];
-    views: Array<{
-        id: string,
-        userId:string
-    }>;
+    userId: string;
+  }>;
+  user: {
+    id: string;
+    name: string;
+  };
 }
 
 export interface AuthType {
-    isAuth: boolean;
-    isLoading: boolean;
+  isAuth: boolean;
+  isLoading: boolean;
 }
-  
+
 export interface GetAuthPesponseType {
-    id:string
-    isAcceptKey:boolean
-    isAuth:boolean
-    name:string
+  id: string;
+  isAcceptKey: boolean;
+  isAuth: boolean;
+  name: string;
 }
 
 export interface IUserInfo {
-    isAuth: boolean,
-    name: string,
-    id: string,
-    isAcceptKey: boolean,
-    role: RoleType
+  isAuth: boolean;
+  name: string;
+  id: string;
+  isAcceptKey: boolean;
+  role: RoleType;
 }
 
 export interface Tag {
-    id: string;
-    title: string;
+  id: string;
+  title: string;
 }
 
 export interface View {
-    id: string;
-    userId: string;
+  id: string;
+  userId: string;
 }
 
 export interface IArticle {
-    date: string; 
-    description: string;
-    id: string;
-    tags: Tag[];
-    title: string;
-    views: View[];
+  date: string;
+  description: string;
+  id: string;
+  tags: Tag[];
+  title: string;
+  views: View[];
 }
