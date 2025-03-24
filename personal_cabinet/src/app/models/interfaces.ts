63b70740597ed951d855c829f1c5d4ef1ef59cbe
IgorFilin <filinigor@yandex.ru>
2025-03-25 01:00:19 +0300

    Правки чата для удаления первого сообщения, при нужном ответе от бека

--- personal_cabinet/src/app/models/interfaces.ts
+++ personal_cabinet/src/app/models/interfaces.ts
@@ -62,12 +62,17 @@ export interface IArticle {
 }
 
 export interface IResponseUserDataWs {
-  Names: IUsersWs[]
-  Message: string
-  Event: string
+  Names: IUsersWs[];
+  Message: IResponseMessage;
+  Event: string;
+}
+
+export interface IResponseMessage {
+  Text: string;
+  IsRemoved: boolean;
 }
 
 export interface IUsersWs {
-  Name:string,
-  Id: string
+  Name: string;
+  Id: string;
 }
